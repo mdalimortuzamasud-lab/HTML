b3a8af6e292b0ce1c71311be392b12e27622d3b6
mdalimortuzamasud-lab <md.alimortuzamasud@gmail.com>
2026-01-08 18:58:38 +0600

    fff

---
+++ javascrift class/javascrift1/main.js
@@ -0,0 +1,10 @@
+function(){
+   
+  let name = document.getElementById("name").value;
+  let remarks =document.getElementById("area").value;
+  let sax=document.querySelector('input[name="gender"]:checked');
+    
+
+
+
+}
