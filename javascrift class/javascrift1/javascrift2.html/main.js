e1102623e970ef67636f0aa7291eb1ef7a73adbc
mdalimortuzamasud-lab <md.alimortuzamasud@gmail.com>
2026-01-11 18:03:11 +0600

    ddd

---
+++ javascrift class/javascrift1/javascrift2.html/main.js
@@ -0,0 +1,15 @@
+function(){
+
+}
+
+let name =document.getElementById('name').value;
+let contact=document.getElementById('contact').value;
+let remarks=document.getElementById('area').value;
+let gender=document.querySelector('input[name="gender"]:checkbox');
+let location=document.querySelector('input[name="location"]:checked');
+let courses =document.querySelectorAll('input[name="course"]:checked');
+
+
+
+
+let result= + name + (/n);
