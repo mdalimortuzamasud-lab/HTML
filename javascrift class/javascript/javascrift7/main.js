32dc67a4d5a3c00f448d9e4b6a2f205ebe7a39c0
mdalimortuzamasud-lab <md.alimortuzamasud@gmail.com>
2026-01-13 19:08:00 +0600

    55

---
+++ javascrift class/javascript/javascrift7/main.js
@@ -0,0 +1,26 @@
+function submitdata(event){
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let contact = document.getElementById("contact").value; 
+    let remarks = document.getElementById("remarks").value; 
+    let location = document.getElementById("location").value; 
+    let gender =document.querySelector('input[name="gender"]:checked');
+    let course =document.querySelectorALL('input[name="course"]:checked');
+
+    let courselist= [];
+     for(let i = 0; i<course.length;i++){
+        courselist.push(course[i].value);
+     }
+    let output = "name:" + name + "\n"+
+                "contact:" + contact + "\n"+
+                "remarks:" + remarks + "\n"+
+                "location:" + location + "\n"+
+                "gender:" + gender.value + "\n"+
+                "course:" + courselist.join(",") + "\n";
+
+    let newWindow =window.open(" ","_blank");
+ newWindow.document.writeln("<pre>" + output + "</pre>");
+
+}
+document.getElementById("myform").addEventListener("submit" , submitdata);
