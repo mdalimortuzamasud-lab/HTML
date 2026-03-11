d5bca2d3294e90c2fd8314088820dc1632c52694
md ali mortuza masud <md.alimortuzamasud@gmail.com>
2026-03-11 10:40:47 +0600

    gfgf

---
+++ oracle/masud.sql
@@ -0,0 +1,8 @@
+CREATE TABLE emp106588 (
+    eid NUMBER(4),
+    ename VARCHAR2(30),
+    salary NUMBER(10,2),
+    hire_date DATE,
+    address VARCHAR2(255),
+    CONSTRAINT emp106588_eid_pk PRIMARY KEY (eid)
+);
